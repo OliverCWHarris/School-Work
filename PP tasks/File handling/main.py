78487b020130870cd59bd5a8160142b52561e13e
Oliver Harris <olivercwharris@gmail.com>
2022-12-07 13:15:00 +0000

    Update main.py

--- PP tasks/File handling/main.py
+++ PP tasks/File handling/main.py
@@ -1,3 +1,13 @@
-with open('films.txt','r') as filmFile:
-    films=filmFile.read()
-    print(films)
+filmfile=open('films.txt','r')
+filmrec=filmfile.readline()
+while filmrec != '':
+    field=filmrec.split(',')
+    id=field[0]
+    title=field[1]
+    rating=field[3]
+    duration=field[4]
+    genre=field[5]
+    if rating=='G':
+        print(id,title,rating,duration)
+    filmrec=filmfile.readline()
+filmfile.close()
